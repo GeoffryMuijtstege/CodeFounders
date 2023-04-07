fddd0dbb33ac39fe2b1ec79a7c17a565a566cc63
GeoffryMuijtstege <67443113+GeoffryMuijtstege@users.noreply.github.com>
2023-04-07 16:09:02 +0200

    First commit

---
+++ Code/simple.py
@@ -0,0 +1,12 @@
+# is voor comments
+
+print("Dit gaat naar de console")
+
+def func1():
+    print("func1 is een functie")
+
+def func2(a):
+    print(a)
+
+func1()
+func2("Zo geef ik een variabele mee")
